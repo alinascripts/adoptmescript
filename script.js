0429b996033389976daddf534ce7b6c18a5fb818
alinascripts <gy.banana.peely@gmail.com>
2025-03-16 16:52:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+document.getElementById("copyBtn").addEventListener("click", function() {
+    let scriptBox = document.getElementById("scriptBox");
+    scriptBox.select();
+    scriptBox.setSelectionRange(0, 99999); // For mobile devices
+    document.execCommand("copy");
+
+    alert("Script copied to clipboard!");
+});
+
